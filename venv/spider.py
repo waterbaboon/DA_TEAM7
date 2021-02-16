3815b997acb79714452b55f0e8ba777513c7cf92
) <74108293+waterbaboon@users.noreply.github.com>
2021-02-16 10:50:56 +0800

    CHANGES

--- venv/spider.py
+++ venv/spider.py
@@ -6,8 +6,8 @@ class QuotesSpider(scrapy.Spider):
 
     def start_requests(self):
         urls = [
-            'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
+            'https://www.goodreads.com/quotes/tag/pages?page=1',
+            'https://www.goodreads.com/quotes/tag/pages?page=2',
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
