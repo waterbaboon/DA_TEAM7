d3d919de97bbeca440e517983b505f8d02492a96
) <74108293+waterbaboon@users.noreply.github.com>
2021-02-16 10:56:33 +0800

    Updates

--- venv/spider.py
+++ venv/spider.py
@@ -14,7 +14,7 @@ class QuotesSpider(scrapy.Spider):
 
     def parse(self, response):
         # page = response.url.split("/")[-2]
-        # filename = 'quotes-{page}.html'
+        # filename = f'quotes-{page}.html'
         # with open(filename, 'wb') as f:
             # f.write(response.body)
         # self.log(f'Saved file {filename}')
