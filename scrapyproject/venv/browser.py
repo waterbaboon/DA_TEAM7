6af79226d8c6731a36e575ce92e5a07fa56b6ecc
) <74108293+waterbaboon@users.noreply.github.com>
2021-02-17 21:45:10 +0800

    Updated logo

--- scrapyproject/venv/browser.py
+++ scrapyproject/venv/browser.py
@@ -1,47 +1,35 @@
-# First HTTP Server
-import socket
-import http.server
-import socketserver
+# Use the Request library
+import requests
 
-# Define the host as a tuple
-HOST, PORT = "", 6969
+# Set the target webpage
+url = 'http://www.twitter.com'
+r = requests.get(url)
 
-handler = http.server.SimpleHTTPRequestHandler
+# This will get the full page
+print(r.text)
 
-with socketserver.TCPServer(("", PORT), handler) as httpd:
-    print("Server started at localhost:" + str(PORT))
-    httpd.serve_forever()
+# This will get the status code
+print("Status code:")
+print("\t *", r.status_code)
+if (r.status_code == 200):
+    print("OK")
 
+# This will just get just the headers
+h = requests.head(url)
+print("Header:")
+print("**********")
 
-class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == '/':
-            self.path = 'index.html'
-        return http.server.SimpleHTTPRequestHandler.do_GET(self)
+# To print line by line
+for x in h.headers:
+    print("\t ", x, ":", h.headers[x])
+print("**********")
 
+# This will modify the headers user-agent
+headers = {
+    'User-Agent' : 'Mobile'
+}
 
-# Create an object of the above class
-handler_object = MyHttpRequestHandler
-
-my_server = socketserver.TCPServer(("", PORT), handler_object)
-
-# Star the server
-my_server.serve_forever()
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.bind((HOST, PORT))
-s.listen(True)
-
-print("Serving HTTP on port %s..." % PORT)
-
-while True:
-    conn, addr = s.accept()
-    rqst = conn.recv(1024)  # Buffer Size
-    print(rqst.decode("utf-8"))  # Display the HTTP request
-    # Define the Web response message
-    resp = 'HTTP/1.0 200 OK\n\nChing Chong Hon Chi'
-    conn.sendall(bytes(resp, "utf-8"))
-    conn.close()
-
-# s.close()  # Closes socket
+# Test it on an external site
+url2 = 'http://httpbin.org/headers'
+rh = requests.get(url2, headers=headers)
+print(rh.text)
--- scrapyproject/venv/server.py
+++ scrapyproject/venv/server.py
@@ -1,35 +1,9 @@
-# Use the Request library
-import requests
+import http.server
+import socketserver
 
-# Set the target webpage
-url = 'http://www.twitter.com'
-r = requests.get(url)
+PORT = 6969
+Handler = http.server.SimpleHTTPRequestHandler
 
-# This will get the full page
-print(r.text)
-
-# This will get the status code
-print("Status code:")
-print("\t *", r.status_code)
-if (r.status_code == 200):
-    print("OK")
-
-# This will just get just the headers
-h = requests.head(url)
-print("Header:")
-print("**********")
-
-# To print line by line
-for x in h.headers:
-    print("\t ", x, ":", h.headers[x])
-print("**********")
-
-# This will modify the headers user-agent
-headers = {
-    'User-Agent' : 'Mobile'
-}
-
-# Test it on an external site
-url2 = 'http://httpbin.org/headers'
-rh = requests.get(url2, headers=headers)
-print(rh.text)
+with socketserver.TCPServer(("", PORT), Handler) as http:
+    print("serving at port", PORT)
+    http.serve_forever()
